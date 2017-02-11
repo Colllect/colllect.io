b47f8e45aee33a162e120e714fe769c1d126099a
Alex-D <contact@alex-d.fr>
2017-02-11 03:48:31 +0100

    feat: add take control section and improve organise

--- js/main.js
+++ js/main.js
@@ -5,14 +5,32 @@ function each (domElementList, callback) {
 }
 
 var activeItemClass = 'm-organise--menu-item-active';
-var activeMenuItem = document.querySelector('.m-organise--menu-item-active-background');
+var activeMenuItem = document.querySelector('.' + activeItemClass);
+var activeMenuBackground = document.querySelector('.m-organise--menu-item-active-background');
 var organiseMenuItems = document.querySelectorAll('.m-organise--menu-item');
+var currentIndex = 0;
+
+activeMenuBackground.style.bottom = activeMenuItem.getBoundingClientRect().height * (organiseMenuItems.length - 1) + 'px';
+
 each(organiseMenuItems, function (item) {
   item.addEventListener('click', function (event) {
     var containerRect = document.querySelector('.m-organise--menu').getBoundingClientRect();
     var currentItem = event.currentTarget;
     var itemRect = currentItem.getBoundingClientRect();
-    activeMenuItem.style.top = itemRect.top - containerRect.top + 'px';
+    var newIndex = [].slice.call(organiseMenuItems).indexOf(currentItem);
+
+    if (currentIndex < newIndex) {
+      activeMenuBackground.style.bottom = itemRect.height * (organiseMenuItems.length - newIndex - 1) + 'px';
+      setTimeout(function () {
+        activeMenuBackground.style.top = itemRect.top - containerRect.top + 'px';
+      }, 75);
+    } else {
+      activeMenuBackground.style.top = itemRect.top - containerRect.top + 'px';
+      setTimeout(function () {
+        activeMenuBackground.style.bottom = itemRect.height * (organiseMenuItems.length - newIndex - 1) + 'px';
+      }, 75);
+    }
+    currentIndex = newIndex;
 
     each(document.querySelectorAll('.' + activeItemClass), function (activeItem) {
       activeItem.classList.remove(activeItemClass);
